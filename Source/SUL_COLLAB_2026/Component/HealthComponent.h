2c775d55ea50e3476399278b225dce2eb0fe3d8f
Lison-Ja <jli094407@gmail.com>
2026-03-06 11:45:39 +0000

    Health Component & FP Template Meshes
    Original Commit messages:
    
    finish Health Comp
    Add Health
    rider
    add more mesh
    add some model
    Quick fixed
    
    Jonah's Refactor:
    Protected Max/Current Health
    Got rid of unnecssary tick method
    Clean up whitespace
    Init MaxHealth to 3
    Set Health to maxHealth on start.
    More concise name for health
    Comment out unnecessary include

---
+++ Source/SUL_COLLAB_2026/Component/HealthComponent.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class SUL_COLLAB_2026_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	int MaxHealth;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int CurrentHealth;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void TakeDamage(int Damage);
+	UFUNCTION(BlueprintCallable)
+	void SetHealth(int NewHealth);
+};
