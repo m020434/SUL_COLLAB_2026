a1fb2ebbc42de90262298c748583573930b896f0
Lison-Ja <jli094407@gmail.com>
2026-03-09 18:55:34 +0000

    Add A Dash

--- Source/SUL_COLLAB_2026/Player/PlayerCharacter.h
+++ Source/SUL_COLLAB_2026/Player/PlayerCharacter.h
@@ -28,15 +28,32 @@ public:
 	UInputAction* LookInputAction;
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")	
 	UInputAction* JumpInputAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Input")
+	UInputAction* DashInputAction;
 	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
 	UHealthComponent* HealthComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Dash")
+	FTimerHandle DashTimer;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Dash")
+	FTimerHandle DashCooldownTimer;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dash")
+	float DashTime;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dash")
+	float DashSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Dash")
+	float DashCooldownTime;
 	
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-
+	bool CanDash = true;
+	bool InDash = false;
+	float StoredSpeed;
+	float StoredGravityScale;
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
@@ -44,10 +61,19 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void DoMove(FVector2D Input);
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void DoLook(FVector2D Input);
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void DoJumpUp(bool Input);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void DoDash(bool Input);
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Dash();
+
+	void EndDash();
+	void ResetDash();
 };
