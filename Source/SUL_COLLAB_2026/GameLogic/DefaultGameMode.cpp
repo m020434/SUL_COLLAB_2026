d7451a3df27df7db456ffe594359ec82c1040188
m020434 <m020434o@student.staffs.ac.uk>
2026-03-10 13:14:59 +0000

    GameManager & Sub-Manager-Scripts
    Created a GameManager UObject class & Submanagers for the timer, score, and multiplier.
    
    GameManager is stored in the Gamemode, and accessible anywhere via a blueprint function library, so designers can access it easily.

--- Source/SUL_COLLAB_2026/GameLogic/DefaultGameMode.cpp
+++ Source/SUL_COLLAB_2026/GameLogic/DefaultGameMode.cpp
@@ -2,8 +2,23 @@
 
 
 #include "DefaultGameMode.h"
+#include "GameManager.h"
 
 ADefaultGameMode::ADefaultGameMode()
 {
-	
+	//gameManager = NewObject<UGameManager>();
 }
+
+void ADefaultGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	gameManager = NewObject<UGameManager>();
+}
+
+void ADefaultGameMode::Tick(float dt)
+{
+	Super::Tick(dt);
+	
+	gameManager->UpdateManager(dt);
+}
+
---
+++ Source/SUL_COLLAB_2026/GameLogic/ManagerClasses/LevelTimeManager.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "LevelTimeManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(F_OnLevelTimerExpire);
+
+UCLASS()
+class SUL_COLLAB_2026_API ULevelTimeManager : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+	ULevelTimeManager();
+	
+	void UpdateManager(float dt);
+
+	#pragma region I/O
+		UFUNCTION(BlueprintCallable)
+		void SetLevelTime( float time );
+		UFUNCTION(BlueprintCallable)
+		void ResetLevelTime();
+		UFUNCTION(BlueprintCallable)
+		void AddLevelTime( float amount ); //Also allows subtraction
+		
+		UFUNCTION(BlueprintPure)
+		float GetLevelTime();
+	#pragma endregion
+	
+	UPROPERTY(BlueprintAssignable)
+	F_OnLevelTimerExpire onLevelTimerExpire;
+	
+protected:
+	float initialLevelTime; //How long the level is (to reset to)
+	float levelTimeRemaining;
+};
