039d658d76731f81599c5f562153fcd06097db2a
sodapants <158485273+sodapants@users.noreply.github.com>
2026-03-19 17:20:08 +0000

    Created base Wildcard class with example.
    Base Wildcard BP Class & Example.
    
    Created a base blueprint class for Wildcards.
    Created an example wildcard based on the Russian roulette wildcard.
    
    Converted into C++ Class
    
    Functionally, everything is the same, just in C++.
    
    Also added a wildcard placeholder texture.

---
+++ Source/SUL_COLLAB_2026/BaseWildcard.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseWildcard.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(F_WildcardFinished);
+
+UCLASS()
+class SUL_COLLAB_2026_API ABaseWildcard : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseWildcard();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Properties
+	UPROPERTY(EditAnywhere) FName Name;
+	UPROPERTY(EditAnywhere) unsigned int Cost;
+	UPROPERTY(EditAnywhere) float Duration;
+	UPROPERTY(EditAnywhere) UTexture2D* CardTexture;
+	
+	// Blueprint Events
+	UFUNCTION(BlueprintImplementableEvent) void OnWildcardActivate();
+	UFUNCTION(BlueprintImplementableEvent) void OnWildcardEnd();
+
+	// Functions
+	UFUNCTION(BlueprintCallable) void RunWildcardTimer();
+
+	// Event Dispatcher
+	UPROPERTY(BlueprintCallable) F_WildcardFinished WildcardFinished;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
